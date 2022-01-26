c980eb192fef783a4eba25b9aea713c3d302c503
Jorge Lisboa <jorgelisboa659@gmail.com>
2022-01-26 12:57:09 -0300

    Users info appears when you write the username.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-import appConfig from '../config.json';
-import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import appConfig from "../config.json";
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { useRouter } from "next/router"; //Hooks
+import React from "react";
 
 function Titulo(props) {
   console.log(props);
-  const Tag = props.tag ||'h1';
+  const Tag = props.tag || "h1";
   return (
     <>
       <Tag>{props.children}</Tag>
       <style jsx>{`
         ${Tag} {
-          color: ${appConfig.theme.colors.neutrals['000']} ;
+          color: ${appConfig.theme.colors.neutrals["000"]};
           font-size: 24px;
           font-weight: 600;
         }
@@ -17,34 +19,7 @@ function Titulo(props) {
     </>
   );
 }
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 //Componente React (Função que retorna HTML)
 //Todos atributos das tags do HTML viram props
 // function HomePage() {
@@ -60,48 +35,93 @@ function GlobalStyle() {
 
 // export default HomePage;
 export default function PaginaInicial() {
-  const username = 'jorgelisboa';
-
+  // const username = 'jorgelisboa';
+  //STATE
+  const [username, setUsername] = React.useState("jorgelisboa");
+  const rout = useRouter();
+  console.log(rout);
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://c4.wallpaperflare.com/wallpaper/756/136/965/fashwave-revolt-glitch-art-vaporwave-statue-hd-wallpaper-preview.jpg)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          backgroundImage:
+            "url(https://c4.wallpaperflare.com/wallpaper/756/136/965/fashwave-revolt-glitch-art-vaporwave-statue-hd-wallpaper-preview.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundBlendMode: "multiply",
         }}
       >
         <Box
           styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
             flexDirection: {
-              xs: 'column',
-              sm: 'row',
+              xs: "column",
+              sm: "row",
             },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            width: "100%",
+            maxWidth: "700px",
+            borderRadius: "5px",
+            padding: "32px",
+            margin: "16px",
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
             backgroundColor: appConfig.theme.colors.neutrals[700],
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infoEvent) {
+              infoEvent.preventDefault();
+              // window.location.href = '/chat'; Dá reload da página
+              //Atualizar para o chatPage
+              rout.push("/chat");
+            }}
             styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: { xs: "100%", sm: "50%" },
+              textAlign: "center",
+              marginBottom: "32px",
             }}
           >
             <Titulo tag="h2">Seja bem vindo!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: "32px",
+                color: appConfig.theme.colors.neutrals[300],
+              }}
+            >
               {appConfig.name}
             </Text>
 
+            {/* <input 
+              type="text"
+              value={username}
+              onChange={function (event){
+                console.log({username} +" digitou", event.target.value)
+                //Cadê o valor?
+                const valor = event.target.value;
+                //Trocar o valor da variable através do react
+                setUsername(valor)
+              }}/> */}
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log({ username } + " digitou", event.target.value);
+                //Cadê o valor?
+                const valor = event.target.value;
+                //Trocar o valor da variable através do react
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -113,8 +133,8 @@ export default function PaginaInicial() {
               }}
             />
             <Button
-              type='submit'
-              label='Entrar'
+              type="submit"
+              label="Entrar"
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
@@ -126,27 +146,26 @@ export default function PaginaInicial() {
           </Box>
           {/* Formulário */}
 
-
           {/* Photo Area */}
           <Box
             styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              maxWidth: "200px",
+              padding: "16px",
               backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: '1px solid',
+              border: "1px solid",
               borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
+              borderRadius: "10px",
               flex: 1,
-              minHeight: '240px',
+              minHeight: "240px",
             }}
           >
             <Image
               styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
+                borderRadius: "50%",
+                marginBottom: "16px",
               }}
               src={`https://github.com/${username}.png`}
             />
@@ -155,8 +174,8 @@ export default function PaginaInicial() {
               styleSheet={{
                 color: appConfig.theme.colors.neutrals[200],
                 backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: '3px 10px',
-                borderRadius: '1000px',
+                padding: "3px 10px",
+                borderRadius: "1000px",
               }}
             >
               {username}

d5e2bce64b73f47a79e1a653012108d07a41b10b
Jorge Lisboa <jorgelisboa659@gmail.com>
2022-01-26 03:29:46 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,170 @@
+import appConfig from '../config.json';
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+
+function Titulo(props) {
+  console.log(props);
+  const Tag = props.tag ||'h1';
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals['000']} ;
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
+    </>
+  );
+}
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
+  );
+}
+//Componente React (Função que retorna HTML)
+//Todos atributos das tags do HTML viram props
+// function HomePage() {
+//   //JSX
+//   return (
+//     <div>
+//       <GlobalStyle />
+//       <Title tag="h2">Seja bem vindo!</Title>
+//       <h2>Discord clone</h2>
+//     </div>
+//   );
+// }
+
+// export default HomePage;
+export default function PaginaInicial() {
+  const username = 'jorgelisboa';
+
+  return (
+    <>
+      <GlobalStyle />
+      <Box
+        styleSheet={{
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage: 'url(https://c4.wallpaperflare.com/wallpaper/756/136/965/fashwave-revolt-glitch-art-vaporwave-statue-hd-wallpaper-preview.jpg)',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        }}
+      >
+        <Box
+          styleSheet={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row',
+            },
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            backgroundColor: appConfig.theme.colors.neutrals[700],
+          }}
+        >
+          {/* Formulário */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+            }}
+          >
+            <Titulo tag="h2">Seja bem vindo!</Titulo>
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              {appConfig.name}
+            </Text>
+
+            <TextField
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[200],
+                  mainColor: appConfig.theme.colors.neutrals[500],
+                  mainColorHighlight: appConfig.theme.colors.primary[660],
+                  backgroundColor: appConfig.theme.colors.primary[600],
+                },
+              }}
+            />
+            <Button
+              type='submit'
+              label='Entrar'
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[550],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[500],
+              }}
+            />
+          </Box>
+          {/* Formulário */}
+
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              maxWidth: '200px',
+              padding: '16px',
+              backgroundColor: appConfig.theme.colors.neutrals[800],
+              border: '1px solid',
+              borderColor: appConfig.theme.colors.neutrals[999],
+              borderRadius: '10px',
+              flex: 1,
+              minHeight: '240px',
+            }}
+          >
+            <Image
+              styleSheet={{
+                borderRadius: '50%',
+                marginBottom: '16px',
+              }}
+              src={`https://github.com/${username}.png`}
+            />
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: '3px 10px',
+                borderRadius: '1000px',
+              }}
+            >
+              {username}
+            </Text>
+          </Box>
+          {/* Photo Area */}
+        </Box>
+      </Box>
+    </>
+  );
+}
